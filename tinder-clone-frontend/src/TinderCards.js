a7ca1b04c3082a800d34fdab7497c60d4fc9a5fb
unknown <alextq3036@gmail.com>
2021-12-29 03:21:37 -0500

    Connected the front end to the backend by getting all the cards from the dta base when the app first loads

--- tinder-clone-frontend/src/TinderCards.js
+++ tinder-clone-frontend/src/TinderCards.js
@@ -1,21 +1,22 @@
 import { SwipeableDrawer } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import TinderCard from 'react-tinder-card'
 import './TinderCards.css'
 
 function TinderCards() {
 
 
-    const [ people, setPeople ] = useState([
-        {
-            name: 'John',
-            url: 'https://images.unsplash.com/photo-1495216875107-c6c043eb703f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-        },
-        {
-            name: 'Matt',
-            url: 'https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+    const [ people, setPeople ] = useState([])
+
+    useEffect(() => {
+        async function fetchData(){
+            const res = await axios.get('http://localhost:8001/tinder/cards')
+            setPeople(res.data)
         }
-    ])
+        fetchData()
+        
+    }, [])
 
     const swiped = ( direction, nameToDelete) => {
         console.log('removing '+ nameToDelete)
@@ -33,13 +34,13 @@ function TinderCards() {
                     return (
                         <TinderCard
                             className='swipe'
-                            key='person.name'
+                            key={person.name}
                             preventSwipe={['up', 'down']}
                             onSwipe={ dir => swiped(dir, person.name)}
                             onCardLeftScreen={() => outOfFrame(person.name)}
                         >
                         <div
-                            style={{backgroundImage: `url("${person.url}")`}}
+                            style={{backgroundImage: `url("${person.imgUrl}")`}}
                             className='card'
                         >
                             <h3>{person.name}</h3>
