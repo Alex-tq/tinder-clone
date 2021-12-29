9375e37ecd2e2261b124dbd7e601f2baac4c7c30
unknown <alextq3036@gmail.com>
2021-12-29 00:20:18 -0500

    Used Mongoosee to stablish a conection to the MongoBb data base

--- tinder-clone-backend/server.js
+++ tinder-clone-backend/server.js
@@ -1,14 +1,22 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()//To have enviroment variables in the .env file
 
 //APP Config
 const app = express()
 const port = process.env.PORT || 8001
+const connection_url = process.env.MONGO_DB_URI;
 
 //Middlewares
 app.use(express.json())
 
 //DB Config
+
+mongoose.connect(connection_url, {
+    useUnifiedTopology: true
+})
 
 //API Endpoints 
 
